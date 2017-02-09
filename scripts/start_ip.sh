89a061849d9716f081a8a5218f27a384d527a959
Denys Vitali <denys.vitali@coelis.ch>
2017-02-09 16:12:28 +0100

    Fix container image

--- scripts/start_ip.sh
+++ scripts/start_ip.sh
@@ -1,1 +1,1 @@
-docker run -it -p 8000:80 --name ip --entrypoint=/start.sh --link ip-mariadb:mysql 76f55509f102
+docker run -it -p 8000:80 --name ip --entrypoint=/start.sh --link ip-mariadb:mysql coelis/invoiceplane:latest
